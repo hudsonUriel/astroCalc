470fa5353e98b9a3d2b3aca195302e3432714990
hudsonUriel <hudsonuriel@gmail.com>
2018-06-25 18:10:44 -0300

    Removing www/

--- www/js/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = window.onresize = function(){
-    astrocalc = document.getElementsByClassName("astrocalc");
-    
-    for(i=0; i<astrocalc.length; i++){
-        astrocalc[i].style.height = window.innerHeight + "px";
-    }
-}
